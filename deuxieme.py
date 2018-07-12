e30c388e8e55a269f11d4892cb776bcbfe60446d
armelbrice <info_brice@yahoo.fr>
2018-07-13 00:39:53 +0200

    Ajout des scripts python simples: premier.py et deuxieme.py

---
+++ deuxieme.py
@@ -0,0 +1,10 @@
+# -*- coding: cp1252 -*-
+#Mon deuxième programme
+liste = []
+compte = int(raw_input('combien de noms ? '))
+for i in range(compte):
+              raw_input('Nom n°'
+			            + str(n + 1)
+						+ ': ').capitalize())
+liste.sort()
+print liste
---
+++ premier.py
@@ -0,0 +1,6 @@
+# -*- coding: cp1252 -*-
+#Mon premier programme
+prenom = raw_input ('Quel est ton prenom ?: ')
+print 'Bonjour ' + prenom + '!'
+raw_input('appuie sur <Entrée> ...')
+
